1ec1a9712c264be6e8b034e70881a2dc0abdfc7c
amiinmuse <amiinmuse2025@u.northwestern.edu>
2024-08-13 21:59:17 -0700

    updated app

--- src/components/Hit.js
+++ src/components/Hit.js
@@ -15,7 +15,8 @@ function Hit() {
         </div>
         <div className="restaurant-list">
           <div className="restaurant-item fast-food">
-            <img src="https://studiokda.com/wp-content/uploads/2014/11/Lazy-Bear-Logo-1300x521.jpg" alt="Lazy Bear" />
+            <img src="https://studiokda.com/wp-content/uploads/2014/11/Lazy-Bear-Logo-1300x521.jpg" alt="Lazy Bear restaurant logo" />
+
             <h3>Lazy Bear</h3>
             <a href="https://www.lazybearsf.com/">Visit</a>
           </div>
@@ -27,12 +28,13 @@ function Hit() {
           <div className="restaurant-item cafe">
             <img src="https://toast-local-nyc3-production.nyc3.cdn.digitaloceanspaces.com/restaurants/1c9420d3-83a8-4036-8d5e-880eb06b4f21/state-bird-provisions-bg-187325-803.webp" alt="State Bird" />
             <h3>State Bird</h3>
-            <a href="#">Visit</a>
+            <a href="https://example.com">Visit</a>
           </div>
           <div className="restaurant-item cafe">
             <img src="https://images.getbento.com/sK5vnQDNSmKFuy5QbKQV_richtable_logo_FINAL_lores.png?w=600&fit=max&auto=compress,format&h=600" alt="Rich Table" />
             <h3>Rich Table</h3>
-            <a href="#">Visit</a>
+            <a href="https://example.com">Visit</a>
+            
           </div>
         </div>
       </section>
--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -8,7 +8,7 @@ function Restaurant() {
         <div className="restaurant-info">
           <div className="restaurant-text">
             <h1>Hazy Dog</h1>
-            <img src="https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1200,height=672,format=auto/https://doordash-static.s3.amazonaws.com/media/restaurant/cover_square/cc876ee7-c57d-4610-81e4-8bcc4394dfd3.png" alt="Restaurant" />
+            <img src="https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1200,height=672,format=auto/https://doordash-static.s3.amazonaws.com/media/restaurant/cover_square/cc876ee7-c57d-4610-81e4-8bcc4394dfd3.png" alt="Hazy Dog restaurant interior" />
             <p>Location: 123 Main St, San Francisco, CA</p>
             <a href="https://www.hayzdogsf.com/">Website (external link)</a>
           </div>
@@ -16,16 +16,16 @@ function Restaurant() {
         <h1>Restaurant Images</h1>
         <div className="menu-images">
           <div className="menu-item">
-            <img src="https://lazydog-assets.s3.amazonaws.com/uploads/nuggets/65a957168fba279df0c7e6bb/POPUP_BannerArtboard_2.jpg" alt="Food Image 1" />
+            <img src="https://lazydog-assets.s3.amazonaws.com/uploads/nuggets/65a957168fba279df0c7e6bb/POPUP_BannerArtboard_2.jpg" alt="Food 1" />
           </div>
           <div className="menu-item">
-            <img src="https://www.eatdrinkoc.com/wp-content/uploads/2013/07/lazydog3.jpg" alt="Food Image 2" />
+            <img src="https://www.eatdrinkoc.com/wp-content/uploads/2013/07/lazydog3.jpg" alt="Food 2" />
           </div>
           <div className="menu-item">
-            <img src="https://res.cloudinary.com/the-infatuation/image/upload/f_auto/q_auto/v1709582984/images/Hayz_Dog_Tramp_1_SF_ERIN_NG_jfsgwn.jpg" alt="Food Image 3" />
+            <img src="https://res.cloudinary.com/the-infatuation/image/upload/f_auto/q_auto/v1709582984/images/Hayz_Dog_Tramp_1_SF_ERIN_NG_jfsgwn.jpg" alt="Food 3" />
           </div>
           <div className="menu-item">
-            <img src="https://s3-media0.fl.yelpcdn.com/bphoto/XwLBJTNtsDCLY00HdYKC9g/348s.jpg" alt="Food Image 4" />
+            <img src="https://s3-media0.fl.yelpcdn.com/bphoto/XwLBJTNtsDCLY00HdYKC9g/348s.jpg" alt="Food 4" />
           </div>
         </div>
       </section>
